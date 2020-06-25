2ad31c3e33b6e8e3d244f18a1b962266cdc1f6e8
L3viathan <git@l3vi.de>
2020-06-25 20:14:38 +0200

    Add auth.py m)

---
+++ apy4i/auth.py
@@ -0,0 +1,36 @@
+import os
+import hmac
+from quart import request, abort
+
+def signing_secret(signing_secret_env):
+    def decorator(afn):
+        async def wrapper(*args, **kwargs):
+            body = await request.get_data()
+            ts = request.headers.get("X-Slack-Request-Timestamp").encode("utf-8")
+            signature = request.headers.get("X-Slack-Signature")
+            version = b"v0"
+            payload = b":".join((version, ts, body))
+            h = hmac.new(
+                os.environ.get(signing_secret_env).encode("utf-8"),
+                payload,
+                hashlib.sha256,
+            )
+            expected = signature[len(version) + 1 :]
+            actual = h.hexdigest()
+            if actual != expected:
+                abort(403)
+            return await afn(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+def simple_token(token_env):
+    def decorator(afn):
+        async def wrapper(*args, **kwargs):
+            body = await request.get_data()
+            token = request.headers.get("X-Token")
+            if os.environ.get(token_env) != token:
+                abort(403)
+            return await afn(*args, **kwargs)
+        return wrapper
+    return decorator
